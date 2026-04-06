b83fa64ee81f940171ff5f0c058ce8b3991a0278
lilyyy411 <lilyyy4111@gmail.com>
2026-04-06 16:02:30 -0400

    Make static images not be framebuffers

--- hypatia/src/pipeline/texture/image.rs
+++ hypatia/src/pipeline/texture/image.rs
@@ -1,44 +1,42 @@
 use std::num::NonZeroU32;
 
 use image::RgbaImage;
-use mini_gl_bindings::Texture2DFrameBuffer;
+use mini_gl_bindings::Texture2D;
 
 use crate::{pipeline::texture::DynamicTexture, util::LogError, window::GlContext};
 
 pub struct ImageTexture {
-    buffer: Texture2DFrameBuffer,
+    buffer: Texture2D,
 }
 impl ImageTexture {
     pub fn new(ctx: &GlContext, image: &RgbaImage) -> eyre::Result<Self> {
         let data = image.pixels().map(|x| x.0).collect::<Vec<_>>();
-        let buffer = ctx
-            .make_texture2d::<mini_gl_bindings::Rgba<u8>>(
-                image
-                    .width()
-                    .try_into()
-                    .log_error("Image width overflowed")?,
-                image
-                    .height()
-                    .try_into()
-                    .log_error("Image height overflowed")?,
-                mini_gl_bindings::PixelFormat::Rgba,
-                Some(&data),
-            )?
-            .make_framebuffer();
+        let buffer = ctx.make_texture2d::<mini_gl_bindings::Rgba<u8>>(
+            image
+                .width()
+                .try_into()
+                .log_error("Image width overflowed")?,
+            image
+                .height()
+                .try_into()
+                .log_error("Image height overflowed")?,
+            mini_gl_bindings::PixelFormat::Rgba,
+            Some(&data),
+        )?;
         Ok(Self { buffer })
     }
 }
 
 impl DynamicTexture for ImageTexture {
     fn dimensions(&self) -> eyre::Result<Option<(std::num::NonZeroU32, std::num::NonZeroU32)>> {
-        let (width, height) = self.buffer.texture().dims();
+        let (width, height) = self.buffer.dims();
         Ok(Some((
             NonZeroU32::new(width.cast_unsigned()).unwrap(),
             NonZeroU32::new(height.cast_unsigned()).unwrap(),
         )))
     }
     fn bind_to_unit(&mut self, unit: usize) -> eyre::Result<()> {
-        self.buffer.texture().bind_to_unit(unit);
+        self.buffer.bind_to_unit(unit);
         Ok(())
     }
     fn fade_focus(
